b33f973725657e18a1f871a504b44597e02e274b
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2026-01-22 22:38:12 +0100

    docs(contracts): add natspec to deployment script

--- contracts/script/DeployDroids.s.sol
+++ contracts/script/DeployDroids.s.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.20;
 import {Script, console} from "forge-std/Script.sol";
 import {Droids} from "../src/Droids.sol";
 
+/**
+ * @title DeployDroids
+ * @dev Script to deploy Droids contract to Base Sepolia
+ */
 contract DeployDroids is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
